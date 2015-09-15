ee3e3d74bf2e8888387015c3878a13d635155c0c
gwansub.oh <gwansub.oh@thinkware.co.kr>
2015-09-15 02:45:15 -0700

    modify the view_setup.c file

--- ezv/app/src/view_setup.c
+++ ezv/app/src/view_setup.c
@@ -19,6 +19,28 @@
  * Variable Declaration
  *
  ******************************************************************************/
+#define SCR_WIDTH						800
+#define SCR_HEIGHT						480
+
+#define TITLE_WIDTH						300
+#define TITLE_HEIGHT					80
+
+#define BUTTON_WIDTH					120
+#define BUTTON_HEIGHT					80
+
+#define BUTTON_VCENTER_1				85		//g_scr_info.rows/4*1 - hButton/2 + 5;
+#define BUTTON_VCENTER_2				240		//g_scr_info.rows/4*2    
+#define BUTTON_VCENTER_3				400		//g_scr_info.rows/4*3 + hButton/2;
+
+#define GRAPH_SPEED						2
+
+static rect_t g_rect_bg;
+static rect_t g_rect_title;
+static rect_t g_rect_board;
+static rect_t g_rect_button[6];
+static rect_t g_rect_graph;
+static GR_WINDOW_ID g_pixmap_board;
+static GR_WINDOW_ID g_pixmap_graph;
 /******************************************************************************
  *
  * Public Functions Declaration
@@ -27,31 +49,10 @@
 void view_setup_init(void);
 
 //------------------------------------------------------------------------------
-// Function Name  : update_gas_info()
+// Function Name  : view_setup_key()
 // Description    :
 //------------------------------------------------------------------------------
-void update_gas_info(void)
-{
-	g_gas_status = g_app_status.gas_stat;
-}
-
-//------------------------------------------------------------------------------
-// Function Name  : diff_gas_info()
-// Description    :
-//------------------------------------------------------------------------------
-u8 diff_gas_info(void)
-{
-	if (g_gas_status != g_app_status.gas_stat)
-		return 1;
-	
-	return 0;
-}
-
-//------------------------------------------------------------------------------
-// Function Name  : view_gas_key()
-// Description    :
-//------------------------------------------------------------------------------
-void view_gas_key(u16 _type, u16 _code)
+void view_setup_key(u16 _type, u16 _code)
 {
 	DBG_MSG_CO(CO_BLUE, "<%s> type: %d, code: %d\r\n", _type, _code);
 
@@ -82,10 +83,10 @@ void view_gas_key(u16 _type, u16 _code)
 }
 
 //------------------------------------------------------------------------------
-// Function Name  : view_gas_entry()
+// Function Name  : view_setup_entry()
 // Description    :
 //------------------------------------------------------------------------------
-void view_gas_entry(void)
+void view_setup_entry(void)
 {
 	PRINT_FUNC_CO();
 
@@ -110,10 +111,10 @@ void view_gas_entry(void)
 }
 
 //------------------------------------------------------------------------------
-// Function Name  : view_gas_draw()
+// Function Name  : view_setup_draw()
 // Description    :
 //------------------------------------------------------------------------------
-void view_gas_draw(void)
+void view_setup_draw(void)
 {
 	if (diff_gas_info() == 0)
 		return;
@@ -134,48 +135,76 @@ void view_gas_draw(void)
 }
 
 //------------------------------------------------------------------------------
-// Function Name  : view_gas_exit()
+// Function Name  : view_setup_exit()
 // Description    :
 //------------------------------------------------------------------------------
-void view_gas_exit(void)
+void view_setup_exit(void)
 {
 	PRINT_FUNC_CO();
 }
 
 //------------------------------------------------------------------------------
-// Function Name  : view_gas_init()
+// Function Name  : view_setup_init()
 // Description    :
 //------------------------------------------------------------------------------
-void view_gas_init(void)
+void view_setup_init(void)
 {	
-	obj_img_t *bg_h = g_gas_bg_h;
-	obj_icon_t *gas_icon_h = g_gas_icon_h;
-	obj_icon_t *sw_icon_h = g_gas_sw_icon_h;
+	g_rect_bg.x = 0;
+	g_rect_bg.y = 0;
+	g_rect_bg.w = g_scr_info.cols;
+	g_rect_bg.h = g_scr_info.rows;
+
+	g_rect_title.x = SCR_WIDTH/2-TITLE_WIDTH/2;
+	g_rect_title.y = 20;
+	g_rect_title.w = TITLE_WIDTH;
+	g_rect_title.h = TITLE_HEIGHT;
+
+	g_rect_board.x = BUTTON_WIDTH+20;
+	g_rect_board.y =  TITLE_HEIGHT+40;
+	g_rect_board.w = SCR_WIDTH - (BUTTON_WIDTH+20)*2;
+	g_rect_board.h = BUTTON_VCENTER_3-(TITLE_HEIGHT+40)+BUTTON_HEIGHT/2;
+
+	g_rect_button[0].x = 0;
+	g_rect_button[0].y = BUTTON_VCENTER_1-BUTTON_HEIGHT/2;
+	g_rect_button[0].w = BUTTON_WIDTH;
+	g_rect_button[0].h = BUTTON_HEIGHT;
+
+	g_rect_button[1].x = 0;
+	g_rect_button[1].y = BUTTON_VCENTER_2-BUTTON_HEIGHT/2;
+	g_rect_button[1].w = BUTTON_WIDTH;
+	g_rect_button[1].h = BUTTON_HEIGHT;
+
+	g_rect_button[2].x = 0;
+	g_rect_button[2].y = BUTTON_VCENTER_3-BUTTON_HEIGHT/2;
+	g_rect_button[2].w = BUTTON_WIDTH;
+	g_rect_button[2].h = BUTTON_HEIGHT;
+
+	g_rect_button[3].x = SCR_WIDTH-1-BUTTON_WIDTH;
+	g_rect_button[3].y = BUTTON_VCENTER_1-BUTTON_HEIGHT/2;
+	g_rect_button[3].w = BUTTON_WIDTH;
+	g_rect_button[3].h = BUTTON_HEIGHT;
+
+	g_rect_button[4].x = SCR_WIDTH-1-BUTTON_WIDTH;
+	g_rect_button[4].y = BUTTON_VCENTER_2-BUTTON_HEIGHT/2;
+	g_rect_button[4].w = BUTTON_WIDTH;
+	g_rect_button[4].h = BUTTON_HEIGHT;
+
+	g_rect_button[5].x = SCR_WIDTH-1-BUTTON_WIDTH;
+	g_rect_button[5].y = BUTTON_VCENTER_3-BUTTON_HEIGHT/2;
+	g_rect_button[5].w = BUTTON_WIDTH;
+	g_rect_button[5].h = BUTTON_HEIGHT;
+
+	g_rect_graph.x = g_rect_board.x+20;
+	g_rect_graph.y = g_rect_board.y+20;
+	g_rect_graph.w = g_rect_board.w-40;
+	g_rect_graph.h = g_rect_board.h-40;
+
+	g_pixmap_board = GrNewPixmap(g_rect_board.w-2, g_rect_board.h-2, NULL);	//for WallPad Serial Dump
+	g_pixmap_graph = GrNewPixmap(g_rect_graph.w, g_rect_graph.h, NULL);	//for Microwave Graph
 	
-	// back ground image
-	bg_h = ui_create_img_obj(0, 0, g_scr_info.cols, g_scr_info.rows,
-				"/app/img/blank_bg.png");
-	// gas icon 
-	gas_icon_h = ui_create_icon_obj(58, 50, 158, 158);
-	if (gas_icon_h) {
-		ui_load_icon_img(gas_icon_h, ICON_IMG_GAS_ON,
-			"/app/img/icon_gas_on.png");
-		ui_load_icon_img(gas_icon_h, ICON_IMG_GAS_OFF,
-			"/app/img/icon_gas_off.png");
-	}
+	ui_register_view(VIEW_ID_SETUP, view_setup_entry,
+		view_setup_draw, view_setup_exit);
 
-	// switch icon 
-	sw_icon_h = ui_create_icon_obj(312, 287, 299, 116);
-	if (sw_icon_h) {
-		ui_load_icon_img(sw_icon_h, ICON_IMG_GAS_SW_ON,
-			"/app/img/icon_switch_on.png");
-		ui_load_icon_img(sw_icon_h, ICON_IMG_GAS_SW_OFF,
-			"/app/img/icon_switch_off.png");
-	}
-
-	ui_register_view(VIEW_ID_GAS, view_gas_entry,
-		view_gas_draw, view_gas_exit);
-
-	hcm_register_key_handler(VIEW_ID_GAS, view_gas_key);
+	hcm_register_key_handler(VIEW_ID_SETUP, view_setup_key);
 }
 
